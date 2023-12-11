e0bd266138cf69f60e358d97405616472a43c7d4
ДимаПрибытков <d.pribytkov93@mail.ru>
2023-12-11 23:24:05 +0300

    3 задания

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,37 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
-        //todo
+        int[] array = new int[10];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        array[5] = 6;
+        array[6] = 7;
+        array[7] = 8;
+        array[8] = 9;
+        array[9] = 10;
+
 
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        //todo
+        /*for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+*/
+
+
+
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
-        //todo
+        for (int i = 9; i <= array.length; i--) {
+            System.out.println(array[i]);
+        }
 
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
-    }
 }
+        }
+
