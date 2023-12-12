db7379b37ae50b45a40942eced1c0650e07dd655
ДимаПрибытков <d.pribytkov93@mail.ru>
2023-12-12 23:38:30 +0300

    2 задания из трех

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         int[] array = new int[10];
+
         array[0] = 1;
         array[1] = 2;
         array[2] = 3;
@@ -15,20 +16,15 @@ public class Main {
         array[8] = 9;
         array[9] = 10;
 
-
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        /*for (int i = 0; i < array.length; i++) {
+       /* for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
-        }
-*/
-
-
-
+        }*/
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
-        for (int i = 9; i <= array.length; i--) {
+       /* for (int i = 9; i <= array.length && i >=0; i--) {
             System.out.println(array[i]);
-        }
+        }*/
 
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
