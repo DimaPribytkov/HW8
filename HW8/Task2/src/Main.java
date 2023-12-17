0874203469560cfb8c123b164cb40bd598c44ddd
ДимаПрибытков <d.pribytkov93@mail.ru>
2023-12-17 22:44:59 +0300

    Генерация веса

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,7 +1,27 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[5];
+        double[] weights = new double[10];
 
-        System.out.println (array[2]);
+        for (int i = 0; i < weights.length; i++) {
+            weights[i] = (new Random().nextDouble() * 60) + 40;
+
+        }
+        double middleWeights = 0;
+        for (int i = 0; i < weights.length; i++) {
+            middleWeights += weights[i];
+
+        }
+        int peopleCount = 0;
+        for (int i = 0; i < weights.length; i++) {
+            if (weights[i] >= 60 && weights[i] <= 80){
+                peopleCount++;
+            }
+
+        }
+        System.out.println("Средний вес = " + middleWeights/weights.length);
+        System.out.println("Колличество людей = " + peopleCount);
+
     }
 }
