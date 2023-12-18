f228b3ffdddf6372610c85546bd460291d6b4241
ДимаПрибытков <d.pribytkov93@mail.ru>
2023-12-18 21:12:00 +0300

    Рефакторинг кода

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -3,22 +3,14 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         double[] weights = new double[10];
-
-        for (int i = 0; i < weights.length; i++) {
-            weights[i] = (new Random().nextDouble() * 60) + 40;
-
-        }
         double middleWeights = 0;
-        for (int i = 0; i < weights.length; i++) {
-            middleWeights += weights[i];
-
-        }
         int peopleCount = 0;
         for (int i = 0; i < weights.length; i++) {
+            weights[i] = (new Random().nextDouble() * 60) + 40;
+            middleWeights += weights[i];
             if (weights[i] >= 60 && weights[i] <= 80){
                 peopleCount++;
             }
-
         }
         System.out.println("Средний вес = " + middleWeights/weights.length);
         System.out.println("Колличество людей = " + peopleCount);
